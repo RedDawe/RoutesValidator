6d3ab0d1887f088fc06b72f95ab62fdeaa34b488
David Dvorak <xdvora10@fi.muni.cz>
2023-10-04 19:01:07 +0200

    fix files api

---
+++ app/src/main/java/cz/dd/routesvalidator/LocationCapturingManager.kt
@@ -0,0 +1,16 @@
+package cz.dd.routesvalidator
+
+class LocationCapturingManager {
+    companion object {
+
+        @Volatile
+        private var instance: LocationCapturingManager? = null
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: LocationCapturingManager().also { instance = it }
+            }
+    }
+
+    var keepCapturing = false
+}
