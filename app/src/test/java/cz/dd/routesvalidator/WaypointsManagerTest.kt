4db697aab0b7f6f7eddf4d27ebffba47f93fa7ed
David Dvorak <xdvora10@fi.muni.cz>
2023-10-24 10:11:05 +0200

    prettify singletons

--- app/src/test/java/cz/dd/routesvalidator/WaypointsManagerTest.kt
+++ app/src/test/java/cz/dd/routesvalidator/WaypointsManagerTest.kt
@@ -99,7 +99,7 @@ class WaypointsManagerTest {
         for ((capturedWaypoints, expectedRoutes) in waypointsRoutesTestValues) {
             val routes = mutableListOf<Route>()
 
-            val waypointsManager = WaypointsManager.getNewInstanceForTests(true)
+            val waypointsManager = WaypointsManager.getNewInstanceForTests()
             for (capturedWaypoint in capturedWaypoints) {
                 val potentialRoute = waypointsManager.processWaypoint(capturedWaypoint)
                 if (potentialRoute != null) routes.add(potentialRoute)
