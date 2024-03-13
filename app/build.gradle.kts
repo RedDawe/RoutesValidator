db4eb0c553bfe06507fbd9bebe803627cbdb0b6d
David Dvořák <davorak01@gmail.com>
2024-03-13 15:05:34 +0100

    new version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cz.dd.routesvalidator"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.9"
+        versionCode = 11
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
